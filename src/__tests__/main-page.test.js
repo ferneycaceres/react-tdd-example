69e677965a6aa787ce2691a925d6f1d684a4458f
Cáceres Suárez <fycz@gft.com>
2022-08-05 12:15:59 +0200

    create basic unit test using jest and react testing library

---
+++ src/__tests__/main-page.test.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+
+import { MainPage } from "../components/main-page"
+
+beforeEach(() => render(<MainPage />))
+
+describe("Main Page mount", () => {
+  it("must display the main page title", () => {
+    expect(
+      screen.getByRole("heading", { name: /simpsons quotes/i })
+    ).toBeInTheDocument()
+  })
+})
+
+describe("Quotes List", () => {
+  it("must display 3 quotes", async () => {
+    expect(await screen.findAllByRole("listitem")).toHaveLength(3)
+  })
+  
+  it("must contain quote value", async () => {
+    const [firstQuote, secondQuote, thirdQuote] = await screen.findAllByRole(
+        "listitem"
+      )
+      expect(firstQuote.textContent).toBe("Gah, stupid sexy Flanders!")
+      expect(secondQuote.textContent).toBe("Eat my shorts")
+      expect(thirdQuote.textContent).toBe(
+        "Shut up, brain. I got friends now. I don't need you anymore"
+      )
+  })
+})
---
+++ src/components/main-page.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const MainPage = () => (
+    <>
+      <h1>Simpsons quotes</h1>
+  
+      <ul>
+        <li>Gah, stupid sexy Flanders!</li>
+        <li>Eat my shorts</li>
+        <li>Shut up, brain. I got friends now. I don't need you anymore</li>
+      </ul>
+    </>
+  )
